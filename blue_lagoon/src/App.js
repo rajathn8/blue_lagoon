3adbae9f821932f3e9c65d8fab788879a339549e
Rajath Rao <rajath@nextorbite.com>
2021-01-18 20:46:22 +0530

    blue lagoon port

--- blue_lagoon/src/App.js
+++ blue_lagoon/src/App.js
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Portfolio Under Development
+          </code> 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/rajath-rao-919431105/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin - Rajath Rao
         </a>
       </header>
     </div>
