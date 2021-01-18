c2b7728d63b0d8210c2719dca5275e0623aa2c7d
Rajath Rao <rajath@nextorbite.com>
2021-01-18 20:58:39 +0530

    blue lagoon ad

--- blue_lagoon/src/App.js
+++ blue_lagoon/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          /Portfolio Under Development
+          Portfolio Under Development<code></code>
         </p>
         <a
           className="App-link"
